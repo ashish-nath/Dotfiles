3d18284deb316c92d3898b38d9442e545fe8f85f
Ashish Nath <84774484+ashish-nath@users.noreply.github.com>
2025-03-17 07:52:37 +0530

    updates

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,47 @@
+-- Always show relative line numbers
+vim.opt.number = true 
+vim.opt.relativenumber = true
+
+--Show line under cursor 
+vim.opt.cursorline = true
+
+vim.opt.expandtab = true --Convert tabs to space
+vim.opt.shiftwidth = 4 --Amount to indent with << and >>
+vim.opt.tabstop = 4 --How many spaces are shown per Tan
+vim.opt.softtabstop  = 4 --How many spaces are applied whenpressing Tab
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true --Keep identation from previous line
+
+--Enable break indent
+vim.opt.breakindent = true
+
+--Store undos between sessions
+vim.opt.undofile = true
+
+--Enable mouse mode, can be useful for resizing splits 
+vim.opt.mouse = "a"
+
+--Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+--Case-insensitve searching UNLESS \C or one or more captial letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+--Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+--Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+--Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 5
---
+++ nvim/lua/plugins/sleuth-vim.lua
@@ -0,0 +1,5 @@
+return{
+    "tpope/vim-sleuth",
+    --No furthur initialization neeeded, as this is a real "vim" not a lua
+    --plugin.
+}
