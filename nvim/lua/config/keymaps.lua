8e598bf6b34e626ad3dfa846f5553ac7a895b0eb
Ashish Nath <84774484+ashish-nath@users.noreply.github.com>
2025-03-28 09:32:08 +0530

    updates new lua files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n","-","<cmd>Oil --float<CR>", {desc="Open the parent directory in Oil"})
+vim.keymap.set("n","gl",function() vim.diagnostic.open_float() end,
+  {desc="Open Diagnostics in Float"}
+)
